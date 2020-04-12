1231f55c7dc05907ef00aae40b090f5c113d53d4
freak4pc <freak4pc@gmail.com>
2020-04-12 23:53:37 +0300

    Update Swift Tools to 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -19,14 +19,17 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
         .package(url: "https://github.com/Quick/Quick", from: "2.0.0"),
         .package(url: "https://github.com/Quick/Nimble", from: "8.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.5.0"),
+        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.5.0"),
         .package(url: "https://github.com/BuzzFeed/MockDuck", .branch("master")),
         .package(url: "https://github.com/jpsim/Yams", from: "2.0.0")
     ],
     targets: [
         .target(
             name: "prism",
-            dependencies: ["PrismCore", "ArgumentParser"],
+            dependencies: [
+                "PrismCore",
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ],
             path: "Sources/CLI"
         ),
         .target(
