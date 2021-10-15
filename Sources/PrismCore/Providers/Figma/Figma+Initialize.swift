b0063f3955b24f57397397fab254d7d6e2e7ef43
Shai Mishali <freak4pc@gmail.com>
2021-10-15 17:39:15 +0300

    Wrap up init figma

--- Sources/PrismCore/Providers/Figma/Figma+Initialize.swift
+++ Sources/PrismCore/Providers/Figma/Figma+Initialize.swift
@@ -11,6 +11,55 @@ import FigmaSwift
 
 extension Figma {
     public static func initialize() throws -> Configuration {
-        return .init(files: [])
+        print("""
+
+                   =+++++=======
+                  +++++++========
+                  +++++++=Figma====
+                   :+++++=======
+                     .+++    .
+                   -=====  .====
+                  ======= =======     Figma ➡ Prism = 🌈 🎨
+                  ======= =======
+                   -=====  .====
+                     .===
+                   .=====
+                  =======
+                  =======
+                   -====
+
+        ┌────────────────────────────────────────────────────────────┐
+        │ NOTE: Figma doesn't have a way to list all available       │
+        │ files. Instead, simply go to Figma on the web, and paste   │
+        │ in URLs to files whose styles you wish to fetch.           │
+        │                                                            │
+        │ * Figma URL look like so:                                  │
+        │ https://www.figma.com/file/[fileID]/[fileName]             │
+        └────────────────────────────────────────────────────────────┘
+        """)
+
+        let urlRegex = try NSRegularExpression(
+            pattern: "^https\\:\\/\\/(www\\.)?figma\\.com\\/file\\/(.*?)(\\/(.*?))?$",
+            options: .caseInsensitive
+        )
+
+        let files: [String] = UserInput(message: "🗳 Figma File URLs")
+            .request(validatingResult: { possibleURL in
+                let trimmed = possibleURL.trimmingCharacters(in: .whitespacesAndNewlines)
+
+                guard let match = urlRegex.firstMatch(
+                        in: trimmed,
+                        options: .init(),
+                        range: NSRange(location: 0, length: trimmed.utf16.count
+                      )),
+                      match.numberOfRanges == 3 || match.numberOfRanges == 5,
+                      let range = Range(match.range(at: 2), in: trimmed) else {
+                  return nil
+                }
+
+                return String(trimmed[range])
+            })
+        
+        return .init(files: files)
     }
 }
